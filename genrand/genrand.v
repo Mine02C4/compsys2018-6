ab62caccf389de2c7fe33473b4a2edb684e6d423
kiyoshirookada <kiyoshirookada@keio.jp>
2018-07-05 12:10:38 +0900

    make random number generator

---
+++ genrand/genrand.v
@@ -0,0 +1,40 @@
+module genrand(clk,rst,seed,randnum);
+   input clk,rst;
+   input [15:0] seed;
+   output [15:0] randnum;
+   reg [15:0] xx;
+   reg [15:0] randnum;
+
+   always @(posedge clk)begin
+      if(rst)begin//はじめだけseed値を代入する
+	 randnum=seed;
+      end
+      xx=randnum;
+      xx=xx^xx<<3;
+      xx=xx^xx>>11;
+      xx=xx^xx<<7;
+      randnum=xx;
+   end
+endmodule // genrand
+
+module test;
+   reg clk,rst;
+   reg [15:0] seed;
+   wire [15:0] randnum;
+   genrand genrand(clk,rst,seed,randnum);
+   always #5 clk=~clk;
+   initial begin
+      $dumpfile("genrand.vcd");
+      $dumpvars(0,test);
+      rst=1;
+      clk=0;
+      seed=4;
+      #20;
+      rst=0;
+      #500;
+      $finish;
+   end // initial begin
+endmodule // test
+
+	
+   

f8027d145bb8b217f37e1edb09242be17ae477c0
naoya <mine@mine-studio.com>
2018-07-16 13:58:26 +0900

    Using argparse for fileio

--- input/dmem_converter.py
+++ input/dmem_converter.py
@@ -1,23 +1,22 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*- 
 
-print('問題番号を入力してください(例)01,02,03..:')
-#for row in output_file:
-input_file = raw_input()
-f = open('Q{0}.txt'.format(input_file),'r')
-elements = f.read().split()
+import argparse
+
+parser = argparse.ArgumentParser(description='Problem converter')
+parser.add_argument('input_file', type=argparse.FileType('r'), metavar='PATH')
+parser.add_argument('-o', type=argparse.FileType('w'), metavar='PATH', dest='output_file')
+args = parser.parse_args()
+elements = args.input_file.read().split()
 if len(elements) % 4 != 0:
     elements.extend(["0"] * (4- len(elements) % 4))
-print(elements)
-output_file = open('dmem{0}.dat'.format(input_file),'w')
 two_elements = iter(elements)
 for first, second, third, fourth in zip(two_elements,two_elements,two_elements,two_elements):
-    output_file.write(format(int(first), '02x'))
-    output_file.write(format(int(second), '02x'))
-    output_file.write(format(int(third), '02x'))
-    output_file.write(format(int(fourth), '02x'))
-    output_file.write("\r\n")
-output_file.close()
-f.close()
-print('output file is dmem{0}.dat'.format(input_file))
+    args.output_file.write(format(int(first), '02x'))
+    args.output_file.write(format(int(second), '02x'))
+    args.output_file.write(format(int(third), '02x'))
+    args.output_file.write(format(int(fourth), '02x'))
+    args.output_file.write("\r\n")
+args.output_file.close()
+print('output file is dmem{0}.dat'.format(args.input_file.name))
 

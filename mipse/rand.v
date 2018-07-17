ef56ca5c1aaadf36e3eeaefae7d61b50729c1b16
takiyu <takiyu1025.txu.development@gmail.com>
2018-07-18 00:12:50 +0900

    Adjust rand seed.

--- mipse/rand.v
+++ mipse/rand.v
@@ -6,7 +6,7 @@ module rand(clk, rst_n, out);
     reg [`DATA_W-1:0] out;
 
     wire [`DATA_W-1:0] seed;
-    assign seed = 32'b11001101101010011101010010101111;
+    assign seed = 32'b11101011101011011101010010101001;
 
     wire [`DATA_W-1:0] x1, x2, x3;
     assign x1 = out ^ out << 13;  // Use previous output

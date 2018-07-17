a84ad9d9ef249e69e25485531563ecbc108f89d5
naoya <mine@mine-studio.com>
2018-07-17 16:24:43 +0900

    Add ;

--- mipse/mem_converter.py
+++ mipse/mem_converter.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', type=argparse.FileType('r'), metavar='PATH', dest='template')
     parser.add_argument('-o', type=argparse.FileType('w'), metavar='PATH', dest='outfile')
     args = parser.parse_args()
-    ostr = '$readmemh("{}", mem)'.format(args.imemdat)
+    ostr = '$readmemh("{}", mem);'.format(args.imemdat)
     text = args.template.read().replace("PLACEHOLDER", ostr)
     args.outfile.write(text)
 

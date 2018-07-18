642c758bdde11efdd10987d2bc80bce4e669a8b9
Naoya Niwa <mine@mine-studio.com>
2018-07-18 15:21:12 +0900

    Revert "use read memh"
    This reverts commit 9bb0dc1ea0d6e377aea3bfc8be4d2187eeaaab35.
    
    やっぱり埋め込まないとVivadoとの相性が悪かった…

--- mipse/mem_converter.py
+++ mipse/mem_converter.py
@@ -5,11 +5,14 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='imem converter')
-    parser.add_argument('imemdat', metavar='PATH')
+    parser.add_argument('imemdat', type=argparse.FileType('r'), metavar='PATH')
     parser.add_argument('-t', type=argparse.FileType('r'), metavar='PATH', dest='template')
     parser.add_argument('-o', type=argparse.FileType('w'), metavar='PATH', dest='outfile')
     args = parser.parse_args()
-    ostr = '$readmemh("{}", mem);'.format(args.imemdat)
+    ostr = ""
+    for i, line in enumerate(args.imemdat):
+        line = line.replace('\n', '')
+        ostr += ("mem[{}] <= 32'h{};\n".format(i, line))
     text = args.template.read().replace("PLACEHOLDER", ostr)
     args.outfile.write(text)
 

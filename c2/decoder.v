8c770ae82179928d816f428a466fd11891f7e95f
naoya <mine@mine-studio.com>
2018-07-20 10:43:27 +0900

    Remove unnecessary files

--- c2/decoder.v
+++
@@ -1,9 +0,0 @@
-module decoder();
-/*	assign iv = d[23:8];
-	assign as = d[7]; ms, mwe, rwe,
-	assign aaddr = d[6:5];
-	assign baddr = d[4:3];
-	assign op = d[2];
-	assign oaddr = d[1:0];
-*/
-endmodule
